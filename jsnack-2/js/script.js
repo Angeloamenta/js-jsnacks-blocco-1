fed69298499504c00a506cc8cd55a650351017e7
Angeloamenta <angeloamenta7@gmail.com>
2021-11-26 15:21:17 +0100

    jsnack-2

---
+++ jsnack-2/js/script.js
@@ -0,0 +1,27 @@
+// jsnack2
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+
+// nomi contenuti dentro l'array
+
+const names = ["angelo", "paolo", "maria", "piero", "gianni"];
+
+// chiedo all'utente il suo nome
+ let yourName = prompt("dimmi il tuo nome");
+
+// cerco dentro alla lista il nome dell'utente
+let ind = false
+for (let i = 0; i < names.length; i++) {
+    let correctName = names[i];
+    console.log(correctName);
+    
+    if (yourName == correctName) {
+        ind = true
+    }
+}
+
+if (ind == true) {
+    alert("sei in lista");
+}else {
+    alert("non ci sei");
+}
